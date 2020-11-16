e1a2de4ddb0cb1816462da27d217b83e58c97365
repl.it user <replituser@example.com>
2020-11-16 12:22:04 +0000

    Added code to display an ASCII robot with dynamic eyes.

---
+++ basics/input/ascii_robot.py
@@ -0,0 +1,11 @@
+# Display a box
+print("Box")
+print("\n")
+# Ask user for eye character
+print("Please enter eye character")
+eye = input()
+print("###########")
+print("#  " + eye + "  " + eye + "   #")
+print("#  ____   #")
+print("###########")
+print("\n")
